779b08e6921cff65abbfb1804a2df9c6f0d8ecf3
Khiem Ton That <29520244+th4tkh13m@users.noreply.github.com>
2021-05-18 21:53:04 +0700

    Create eulerAlgorithm.py

---
+++ Number Theory/eulerAlgorithm.py
@@ -0,0 +1,8 @@
+def euler(a, b):
+    """a and b are integers.
+    Return the greatest common divisor of a and b."""
+    if b % a == 0:
+        return a
+    return euler(b % a, a)
+
+print(euler(3, 5))
